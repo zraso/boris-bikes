8704a775a2e43487512491d0c25f2b5dda07785e
zraso <zaira.rasool@hotmail.co.uk>
2019-09-17 16:52:40 +0100

    Add feature tests for bike working

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,8 @@
+require_relative 'bike'
+
 class DockingStation
   def release_bike
-
+    Bike.new
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,6 @@
 require 'docking_station'
+#
+# require 'bike'
 
 describe DockingStation do
   # it 'responds to release_bike' do
@@ -7,4 +9,11 @@ describe DockingStation do
 
   # Rspec oneliner syntax:
   it {is_expected.to respond_to :release_bike}
+
+  it 'releases working bike' do
+    # expect(subject.release_bike).to respond_to :working?
+
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
