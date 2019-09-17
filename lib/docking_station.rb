e567aae1828b3be82ddd21455692c7e6a1c80c97
zraso <zaira.rasool@hotmail.co.uk>
2019-09-17 15:57:02 +0100

    Add Bike class and working method with rspec tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,2 +1,6 @@
 class DockingStation
+  def release_bike
+
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,10 @@
-require 'dockingstation'
+require 'docking_station'
 
 describe DockingStation do
+  # it 'responds to release_bike' do
+  #   expect(subject).to respond_to :release_bike
+  # end
 
+  # Rspec oneliner syntax:
+  it {is_expected.to respond_to :release_bike}
 end
