9084f0654597cd206eb34297a263e2a3057420f8
zraso <zaira.rasool@hotmail.co.uk>
2019-09-18 15:41:10 +0100

    Add instance variable and errors raised

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,8 +1,29 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike
+  # def initialize
+  #   @bike = nil
+  # end
+  #
+  # def bike
+  #   @bike
+  # end
+
   def release_bike
-    Bike.new
+    if @bike == nil
+      raise "NoBikeClass"
+    else
+      Bike.new
+    end
+  end
+
+  def dock_bike(bike)
+    if @bike == nil
+      @bike = bike
+    else
+      raise "BikeAlreadyDocked"
+    end
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,17 +3,36 @@ require 'docking_station'
 # require 'bike'
 
 describe DockingStation do
+  #set docked_station variable at the start of unit tests that can then be used
+  # by other tests
+    docked_station = DockingStation.new
+    undocked_station = DockingStation.new
+    docked_station.dock_bike(Bike.new)
+    # docked_station.dock_bike(Bike.new)
   # it 'responds to release_bike' do
   #   expect(subject).to respond_to :release_bike
   # end
 
   # Rspec oneliner syntax:
-  it {is_expected.to respond_to :release_bike}
+  describe '#release_bike' do
+    it {is_expected.to respond_to :release_bike}
 
-  it 'releases working bike' do
-    # expect(subject.release_bike).to respond_to :working?
+    it 'releases working bike' do
+      # expect(subject.release_bike).to respond_to :working?
+      bike = docked_station.release_bike
+      expect(bike).to respond_to :working?
+    end
 
-    bike = subject.release_bike
-    expect(bike).to be_working
+    it 'raises an error if no bikes are available' do
+      expect {undocked_station.release_bike}.to raise_error("NoBikeClass")
+    end
+  end
+
+  describe '#dock_bike' do
+    it {expect(docked_station.bike).to be_a Bike}
+
+    it 'raises an error if bike is already docked' do
+      expect {docked_station.dock_bike(Bike.new)}.to raise_error("BikeAlreadyDocked")
+    end
   end
 end
